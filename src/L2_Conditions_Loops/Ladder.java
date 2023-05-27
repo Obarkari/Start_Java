a117e4e8dc9bc36846848468a2d4b5f34a7bd8dd
Obarkari <ms.barkar@gmail.com>
2023-05-27 07:45:00 +0300

    - Add Guess1.

--- src/L2_Conditions_Loops/Ladder.java
+++ src/L2_Conditions_Loops/Ladder.java
@@ -1,4 +1,17 @@
-package L2;
+package L2_Conditions_Loops;
 
 public class Ladder {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 6; x++) {
+            if (x==1) System.out.println("x is egual to one. ");
+            else if (x==2) System.out.println("x is egual to two. ");
+            else if (x==3) System.out.println("x is egual to three. ");
+            else if (x==4) System.out.println("x is egual to four. ");
+            else System.out.println("x out of diapason 1-4");
+
+
+        }
+
+    }
 }
