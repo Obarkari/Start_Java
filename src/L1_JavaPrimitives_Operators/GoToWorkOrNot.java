0e2d816db12ea4642922b72ef2313b88cc4c9ee4
Obarkari <ms.barkar@gmail.com>
2023-05-27 09:20:38 +0300

    - Add GoToWorkOrNot.java

---
+++ src/L1_JavaPrimitives_Operators/GoToWorkOrNot.java
@@ -0,0 +1,22 @@
+package L1_JavaPrimitives_Operators;
+
+import java.util.Scanner;
+
+public class GoToWorkOrNot {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Today is weekday? Answer true or false.");
+        boolean isWeekday = input.nextBoolean();
+
+        System.out.println("Today is holiday? Answer true or false");
+        boolean isHoliday = input.nextBoolean();
+
+        if (isWeekday & isHoliday) System.out.println("It is not weekend bat you can stay home, becous...");
+        if (isWeekday | isHoliday) System.out.println("It is weekend or holiday, so stay home.");
+        if (isWeekday & isHoliday) System.out.println("It is weekend and not holiday, so go to work.");
+
+
+    }
+}
