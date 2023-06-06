477f5d4fbe02799e5547ad51e46d6093f921a548
Obarkari <ms.barkar@gmail.com>
2023-06-06 21:57:27 +0300

    - Add Help.java

---
+++ src/IndependentWork/PromDemo.java
@@ -0,0 +1,16 @@
+package IndependentWork;
+
+public class PromDemo {
+    public static void main(String[] args) {
+        byte b;
+        int i;
+
+        b = 10;
+        i = b * b;
+
+        b = 10;
+        b = (byte) (b * b);
+
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
