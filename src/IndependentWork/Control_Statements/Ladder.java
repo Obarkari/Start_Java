8f39cece205bfa80281f60543617dc7c3ee7e2c2
Obarkari <ms.barkar@gmail.com>
2023-06-06 21:58:07 +0300

    - Add Help.java

---
+++ src/IndependentWork/Control_Statements/Ladder.java
@@ -0,0 +1,19 @@
+package IndependentWork.Control_Statements;
+
+public class Ladder {
+    public static void main(String[] args) {
+        int x;
+        for (x=0; x<6; x++) {
+            if (x==1)
+                System.out.println("x равно единице");
+            else if (x==2)
+                System.out.println("x равно двум");
+            else if (x==3)
+                System.out.println("x равно трём");
+            else if (x==4)
+                System.out.println("x равно четырём");
+            else
+                System.out.println("Значение x находится вне диапазона 1 - 4");
+        }
+    }
+}
---
+++ src/IndependentWork/Control_Statements/NoBreak.java
@@ -0,0 +1,25 @@
+package IndependentWork.Control_Statements;
+
+public class NoBreak {
+    public static void main(String[] args) {
+        int i;
+
+        for (i=0; i<=5; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i меньше единицы");
+                case 1:
+                    System.out.println("i меньше друх");
+                case 2:
+                    System.out.println("i меньше трёх");
+                case 3:
+                    System.out.println("i меньше четырёх");
+                case 4:
+                    System.out.println("i меньше пяти");
+            }
+                    System.out.println();
+
+            }
+    }
+}
+
