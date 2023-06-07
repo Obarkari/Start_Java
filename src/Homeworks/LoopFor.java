069f5d1b43f0838b6c9e1c21da2136554b2ffcf8
Obarkari <ms.barkar@gmail.com>
2023-06-07 18:56:33 +0300

    - Add LoopFor.java

---
+++ src/Homeworks/LoopFor.java
@@ -0,0 +1,9 @@
+package Homeworks;
+
+public class LoopFor {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 500; i += i)
+            System.out.println("Derivation of a geometric progression term " + i);
+    }
+}
