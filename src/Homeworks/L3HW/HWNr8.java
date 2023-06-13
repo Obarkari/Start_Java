948005207f6cf6157efb3532e47b626c378f3f2a
Obarkari <ms.barkar@gmail.com>
2023-06-13 20:22:05 +0300

    - Add EvenOrOddNumber .java

---
+++ src/Homeworks/L3HW/HWNr8.java
@@ -0,0 +1,14 @@
+package Homeworks.L3HW;
+
+public class HWNr8 {
+    public static void main(String[] args) {
+        int i;
+ //       int continue i;
+
+ //       for (int i = 0; i<10; i++) {
+ //           System.out.println(i + " ");
+ //           if ((i%2) == 0) continue;
+ //           System.out.println(i);
+        }
+    }
+
